74f067eb5ab4b590c2c6b3667decf2f8924a55fc
Johnny Rocket <0.snilcy@gmail.com>
2020-10-27 19:57:13 +0300

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{Component.name}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
